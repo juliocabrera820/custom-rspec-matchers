0c0bb0f5e8c33168ad42fbd05c99745506525bd0
julio cabrera <juliocabrera820@gmail.com>
2021-06-20 01:39:12 -0500

    add number test

---
+++ spec/number_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require_relative './../src/number'
+
+RSpec.describe Number do
+  it 'returns a number multiple of 2' do
+    expect(6).to be_a_multiple_of(2)
+  end
+
+  it 'returns a positive number' do
+    expect(1).to be_positive
+  end
+
+  it 'returns a negative number' do
+    expect(0).not_to be_positive
+  end
+end
